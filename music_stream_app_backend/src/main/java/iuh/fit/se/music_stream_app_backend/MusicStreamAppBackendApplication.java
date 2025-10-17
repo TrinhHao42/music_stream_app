0720478e98225c4a990f1fac4bb3a1b69d9b6b48
Trịnh Quang Toàn Hảo <142070040+TrinhHao42@users.noreply.github.com>
2025-10-17 20:14:32 +0700

    first commit

---
+++ music_stream_app_backend/src/main/java/iuh/fit/se/music_stream_app_backend/MusicStreamAppBackendApplication.java
@@ -0,0 +1,13 @@
+package iuh.fit.se.music_stream_app_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicStreamAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicStreamAppBackendApplication.class, args);
+    }
+
+}
---
+++ music_stream_app_backend/src/test/java/iuh/fit/se/music_stream_app_backend/MusicStreamAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package iuh.fit.se.music_stream_app_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicStreamAppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
